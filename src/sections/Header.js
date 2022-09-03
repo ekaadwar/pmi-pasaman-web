26c6d1a28c518888625d9b963deb50bf73b19d4f
ekaadwar <eka.adwar@gmail.com>
2022-09-03 08:22:55 +0700

    membuat component Container dan inisialisasi section Header, Routes, serta Footer

---
+++ src/sections/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../components/Container";
+
+const Header = () => {
+  return (
+    <header>
+      <Container content={<p>Header</p>} />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/sections/Routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Routes = () => {
+  return (
+    <div>
+      <p>oke</p>
+    </div>
+  );
+};
+
+export default Routes;

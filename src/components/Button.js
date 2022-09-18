50a422769055ffb8d761d99dd871c38111386c2c
ekaadwar <eka.adwar@gmail.com>
2022-09-18 22:54:13 +0700

    responsif pada data page

--- src/components/Button.js
+++ src/components/Button.js
@@ -31,7 +31,7 @@ export const NavButton = ({ active = false, text = "" }) => {
 
 export const ActionButton = ({ content = <div /> }) => {
   return (
-    <button className="button-action flex flex-row items-center justify-center bg-red-900 active:bg-red-700 text-white h-8 px-1 rounded-md">
+    <button className="button-action flex flex-row items-center justify-center bg-red-900 active:bg-red-700 text-white h-8 px-1 w-full sm:w-auto rounded-md">
       {content}
     </button>
   );

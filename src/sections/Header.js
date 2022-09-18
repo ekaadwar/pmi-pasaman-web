90f821905e113981758a0df557fea56a37b6b2c8
ekaadwar <eka.adwar@gmail.com>
2022-09-18 08:08:57 +0700

    instalasi hamburger-react; menerapkan responsif pada header/navbar

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/Container";
-import { logoPmi } from "../assets";
 import { Link } from "react-router-dom";
+import { Sling as Hamburger } from "hamburger-react";
+import { logoPmi } from "../assets";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="fixed z-50 w-full bg-white shadow-md shadow-stone-500/40">
       <Container
@@ -15,12 +17,25 @@ const Header = () => {
               </div>
             </Link>
 
-            <div className="flex flex-row justify-center space-x-8">
+            <div
+              className="lg:hidden"
+              // onClick={() => setIsOpen(!isOpen)}
+            >
+              <Hamburger
+                toggled={isOpen}
+                toggle={() => setIsOpen(!isOpen)}
+                color="rgb(127 29 29)"
+                size={20}
+              />
+            </div>
+
+            <div className="hidden lg:flex flex-row justify-center space-x-8">
               <Link to="#">Tentang Kami</Link>
               <Link to="#">Stok Darah</Link>
               <Link to="#">Pelayanan</Link>
             </div>
-            <div className="flex flex-row justify-center space-x-8">
+
+            <div className="hidden lg:flex flex-row justify-center space-x-8">
               <Link to="signup">Daftar</Link>
               <p>|</p>
               <Link to="signin">Masuk</Link>
@@ -28,6 +43,47 @@ const Header = () => {
           </div>
         }
       />
+
+      {isOpen && (
+        <div
+          id="toggleMenu"
+          className=" block lg:hidden bg-gradient-to-b from-white pb-20"
+        >
+          <Container
+            content={
+              <div className="container mx-auto px-4 lg:px-10 flex flex-col my-5 space-y-5 items-end pr-3">
+                <Link
+                  to={(location) => {
+                    return { ...location, pathname: "/" };
+                  }}
+                  onClick={() => setIsOpen(false)}
+                >
+                  Home
+                </Link>
+                <Link to="#" onClick={() => setIsOpen(false)}>
+                  Tentang Kami
+                </Link>
+                <Link to="#" onClick={() => setIsOpen(false)}>
+                  Stok Darah
+                </Link>
+                <Link to="#" onClick={() => setIsOpen(false)}>
+                  Pelayan
+                </Link>
+
+                <div className="flex flex-row space-x-3 font-bold">
+                  <Link to="/signin" onClick={() => setIsOpen(false)}>
+                    Signin
+                  </Link>
+                  <p>|</p>
+                  <Link to="/signup" onClick={() => setIsOpen(false)}>
+                    Sign Up
+                  </Link>
+                </div>
+              </div>
+            }
+          />
+        </div>
+      )}
     </header>
   );
 };

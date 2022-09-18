2d7675b04ba7b5d896964061c930f29355ad1122
ekaadwar <eka.adwar@gmail.com>
2022-09-18 07:26:11 +0700

    menghilangka history

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,57 +1,58 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import Container from "../../components/Container";
 import { CircleButton } from "../../components/Button";
 import { CircleMd } from "../../components/Circle";
-import { donorHistory } from "../../dummy";
+// import { donorHistory } from "../../dummy";
 import { FiEdit2 as Edit } from "react-icons/fi";
 import { InputArea, InputProfile } from "../../components/Input";
 import { ProfileCard } from "../../components/Card";
 import { zulaikha } from "../../assets";
-import DonorHistory from "../../components/DonorHistory";
+// import DonorHistory from "../../components/DonorHistory";
 
 const Profile = () => {
-  const [leftData, setLeftData] = useState([]);
-  const [rightData, setRightData] = useState([]);
+  // const [leftData, setLeftData] = useState([]);
+  // const [rightData, setRightData] = useState([]);
 
-  useEffect(() => {
-    getData(donorHistory);
-  }, []);
+  // useEffect(() => {
+  //   getData(donorHistory);
+  // }, []);
 
-  const getData = (array) => {
-    const length = array.length;
-    const difference = length - 8;
-    if (difference > 0) {
-      for (let i = 0; i < difference; i++) {
-        array.splice(0, 1);
-      }
-    }
-    let data1 = [];
-    let data2 = [];
+  // const getData = (array) => {
+  //   const length = array.length;
+  //   const difference = length - 8;
+  //   if (difference > 0) {
+  //     for (let i = 0; i < difference; i++) {
+  //       array.splice(0, 1);
+  //     }
+  //   }
+  //   let data1 = [];
+  //   let data2 = [];
 
-    if (length <= 4) {
-      for (let i = 0; i < length; i++) {
-        data1[i] = array[i];
-      }
-    } else {
-      for (let i = 0; i < 4; i++) {
-        data1[i] = array[i];
-      }
-      let j = 0;
-      for (let i = 4; i < length; i++) {
-        data2[j] = array[i];
-        j += 1;
-      }
-    }
+  //   if (length <= 4) {
+  //     for (let i = 0; i < length; i++) {
+  //       data1[i] = array[i];
+  //     }
+  //   } else {
+  //     for (let i = 0; i < 4; i++) {
+  //       data1[i] = array[i];
+  //     }
+  //     let j = 0;
+  //     for (let i = 4; i < length; i++) {
+  //       data2[j] = array[i];
+  //       j += 1;
+  //     }
+  //   }
 
-    console.log("data1");
-    console.log(data1);
-    console.log("data2");
-    console.log(data2);
-    setLeftData(data1);
-    setRightData(data2);
-  };
+  //   console.log("data1");
+  //   console.log(data1);
+  //   console.log("data2");
+  //   console.log(data2);
+  //   setLeftData(data1);
+  //   setRightData(data2);
+  // };
 
   return (
     <section className="pt-32 pb-20">
@@ -133,7 +134,7 @@ const Profile = () => {
               </div>
             </div>
 
-            <div className="flex flex-col items-center mt-20 space-y-5">
+            {/* <div className="flex flex-col items-center mt-20 space-y-5">
               <div className="flex flex-row justify-center items-start w-full max-w-2xl">
                 <div
                   className={`flex-1 space-y-4 px-6 ${
@@ -169,7 +170,7 @@ const Profile = () => {
               <div className="py-2 px-3 bg-gray-200 cursor-pointer active:bg-gray-400 rounded-md shadow-md shadow-black">
                 <p className="text-sm">Lihat semua riwayat donor</p>
               </div>
-            </div>
+            </div> */}
           </div>
         }
       />

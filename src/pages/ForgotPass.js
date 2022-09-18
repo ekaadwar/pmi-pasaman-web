ab81c342586e8be5861fe8112f9bd5f465d00d88
ekaadwar <eka.adwar@gmail.com>
2022-09-18 23:12:08 +0700

    membuat responsif pada 'halaman Lupa Password'

--- src/pages/ForgotPass.js
+++ src/pages/ForgotPass.js
@@ -3,19 +3,23 @@ import { SectionHeader } from "../components/Text";
 
 const ForgotPass = () => {
   return (
-    <section className="forgotpass-page flex flex-col justify-center items-center h-screen text-white space-y-5">
+    <section className="forgotpass-page flex flex-col justify-center items-center h-screen text-white space-y-5 px-5 py-20">
       <SectionHeader text="Anda lupa password?" />
-      <p>Jangan khawatir, kami akan menemukannya kembali.</p>
-      <div className="grid grid-cols-3 h-16 w-full max-w-lg space-x-2">
+      <p className="text-center">
+        Jangan khawatir, kami akan menemukannya kembali.
+      </p>
+      <div className="grid sm:grid-cols-3 w-full max-w-lg sm:space-x-2 space-y-4 sm:space-y-0">
         <input
-          className="focus:outline-none text-gray-700 col-span-2 px-2 h-full rounded-l-lg"
+          className="focus:outline-none text-gray-700 sm:col-span-2 px-2 h-16  rounded-l-lg"
           placeholder="Masukan email Anda"
         />
-        <button className="bg-red-900 rounded-r-lg active:bg-red-700">
+        <button className="bg-red-900 rounded-l-lg active:bg-red-700 h-16">
           Kirim
         </button>
       </div>
-      <p>Kami akan segera mengirim link ke alamat email Anda.</p>
+      <p className="text-center">
+        Kami akan segera mengirim link ke alamat email Anda.
+      </p>
     </section>
   );
 };

bff77dd2b311d3d2e102533b11cd678438df77b5
ekaadwar <eka.adwar@gmail.com>
2022-10-13 07:01:06 +0700

    instalasi eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,26 @@ module.exports = {
   ecmaFeatures: {
     legacyDecorators: true,
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    indent: ["off", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-    "react/prop-types": ["off"],
-    "react/no-unescaped-entities": ["off"],
+    indent: ['off', 'tab'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'react/prop-types': ['off'],
+    'react/no-unescaped-entities': ['off'],
   },
-};
+}
--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import { connect, useSelector } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import Footer from "./sections/Footer";
-import Header from "./sections/Header";
-import Routes from "./sections/Routes";
-import Loading from "./components/Loading";
+import './App.css'
+import { connect, useSelector } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Footer from './sections/Footer'
+import Header from './sections/Header'
+import Routes from './sections/Routes'
+import Loading from './components/Loading'
 
 function App(props) {
-  const { isLoading } = useSelector((state) => state.globalReducer);
+  const { isLoading } = useSelector((state) => state.globalReducer)
   return (
     <Router>
       {props.auth.onAuth === false && <Header />}
@@ -15,11 +15,11 @@ function App(props) {
       {props.auth.onAuth === false && <Footer />}
       {isLoading && <Loading />}
     </Router>
-  );
+  )
 }
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
-});
+})
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
